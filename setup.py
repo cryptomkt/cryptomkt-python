72c3309ad47fc7d57339bb365e17512790d3b780
Pedro Pablo Bustamante Barrera <ppbb15@gmail.com>
2019-03-04 16:03:26 -0300

    improve setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,6 @@ import os
 import setuptools
 import sys
 
-here = os.path.abspath(os.path.dirname(__file__))
-
 # 'setup.py publish' shortcut.
 if sys.argv[-1] == 'publish':
     print("publish")
@@ -13,28 +11,31 @@ if sys.argv[-1] == 'publish':
     os.system('twine upload dist/*')
     sys.exit()
 
-with open(os.path.join(here, 'README.md'), 'r') as f:
-    README = f.read()
-
-with open(os.path.join(here, 'requirements.txt'), 'r') as f:
-    REQUIREMENTS = [line.strip() for line in f.readlines()]
+README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+REQUIREMENTS = [
+    line.strip() for line in open(os.path.join(os.path.dirname(__file__),
+                                               'requirements.txt')).readlines()]
 
 setuptools.setup(
     name="cryptomarket",
-    version="0.0.4",
-    author="CryptoMarket",
-    author_email="pablo@cryptomkt.com",
+    version="0.0.5",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
     description="CryptoMarket API client library",
     long_description=README,
     long_description_content_type="text/markdown",
+    keywords=['api', 'cryptomkt', 'cryptomarket', 'bitcoin', 'client'],
     url="https://github.com/cryptomkt/cryptomkt-python",
-    include_package_data=True,
-    packages=setuptools.find_packages(),
     install_requires=REQUIREMENTS,
+    author="CryptoMarket",
+    author_email="pablo@cryptomkt.com",
     classifiers=(
+        "Intended Audience :: Developers",
         "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
     ),
-)    
+)
