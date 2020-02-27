37052965c2b9bd64c222687035c65af85aaba7d9
francosodoxo <franco.seguel@ug.uchile.cl>
2020-02-27 12:27:37 -0300

    Se modifica el socketClient.py y se agrega __init__.py a la carpeta socketio

--- socketio/socketClient.py
+++ socketio/socketClient.py
@@ -1,2 +1,20 @@
 import socketio as s
+import exchange.client as c
+#getting api keys
+keysFile = open("keys.txt", "r")
 
+keys = keysFile.readline().split(",")
+
+client = c.Client(keys[0], keys[1])
+
+print(client)
+
+sockeys = client.get_account()
+
+sio = s.Client()
+
+sio.connect("wss://worker.cryptomkt.com:443")
+
+"""@sio.event
+def message(data):
+    print()"""
