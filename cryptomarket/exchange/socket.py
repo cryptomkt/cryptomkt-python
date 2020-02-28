8dedf0259f80dcf6dfa99e80678517f99c03f94a
T. Ismael <ismael.verdugo.za@gmail.com>
2020-02-28 11:54:34 -0300

    socketio working, not full implemented

--- cryptomarket/exchange/socket.py
+++ cryptomarket/exchange/socket.py
@@ -4,10 +4,14 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 import socketio
+import time
+
+from .client import Client
 
 
 class Socket(object):
     def __init__(self, socketids):
+
         self.socketids = socketids
         sio =  socketio.Client()
 
@@ -24,16 +28,29 @@ class Socket(object):
         def disconnect():
             print('disconnected from server')
 
-        @sio.event
-        def disconnect():
-            print('disconnected from server')
-
         sio.connect('https://worker.cryptomkt.com')
-        
         sio.emit('socket-auth', socketids)
 
-        @sio.on('board')
+        @sio.on('currencies')
+        def currencies_handler(msg):
+            print('currencies received')
+            print(msg)
+    
         def board_handler(msg):
+            print('board received')
+            print(msg)
+            return msg
+        sio.on('board-delta', board_handler)
+
+        @sio.on('open-orders')
+        def open_orders_handler(msg):
+            print('open-orders received')
             print(msg)
 
+        @sio.on('ticker')
+        def ticker_handler(msg):
+            print('ticker received')
+            print(msg)
+
+        # sio.wait()
         self.sio = sio
--- script.py
+++ script.py
@@ -1,6 +1,8 @@
 import cryptomarket.exchange.client as c
+import cryptomarket.exchange.socket as s
 
 client = c.Client("2755964/01BgCj1WCabUw2", "JeufSrS1FoYxD/AOEpRF0XMrwVFFU1OL80qViE94R80Tpz9VZ0Cr5Rl8EXhmX0Yd3HcD89T6S")
 
-instant = client.get_instant('ETHCLP', 'buy', '100000')
-print(instant)
+auth = client.get_auth_socket()
+
+s.Socket(auth)
