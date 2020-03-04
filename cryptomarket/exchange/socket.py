34a970111156f9b567bbdcab263ea2da462f8a1f
T. Ismael <ismael.verdugo.za@gmail.com>
2020-03-04 17:29:12 -0300

    patching implemented

--- cryptomarket/exchange/socket.py
+++ cryptomarket/exchange/socket.py
@@ -5,19 +5,44 @@ from __future__ import unicode_literals
 
 import socketio
 import time
+import logging
+
+debugging = True
+
+logger = logging.getLogger(__name__)
+
+logging.basicConfig(
+    filename='socket.log',
+    format='%(asctime)s [%(levelname)s] %(message)s', 
+    datefmt='%m/%d/%Y %I:%M:%S %p')
+
+if debuging:
+    logger.setLevel(logging.DEBUG)
+else:
+    logger.setLevel(logging.INFO)
+
 
 from .client import Client
 
-
 class Socket(object):
     def __init__(self, socketids):
+        self.url_worker = 'https://worker.cryptomkt.com'
+        self.currencies_data = None
+        self.balances_data = None
+        self.operated_data = None
+        self.open_orders_data = None
+        self.historical_orders_data = None
+        self.open_book_data = None
+        self.historical_book_data = None
+        self.candles_data = None
+        self.board_data = None
 
         self.socketids = socketids
         sio =  socketio.Client()
 
         @sio.event
         def connect():
-            print('connection established')
+            logger.info('connecting socket')
 
         @sio.event
         def my_message(data):
@@ -28,19 +53,17 @@ class Socket(object):
         def disconnect():
             print('disconnected from server')
 
-        sio.connect('https://worker.cryptomkt.com')
+        sio.connect(self.url_worker)
+
+        logger.info('authenticating socket')
         sio.emit('socket-auth', socketids)
 
+
         @sio.on('currencies')
-        def currencies_handler(msg):
-            print('currencies received')
-            print(msg)
+        def currencies_handler(data):
+            log.info('currencies received')
+            print(type(msg))
     
-        def board_handler(msg):
-            print('board received')
-            print(msg)
-            return msg
-        sio.on('board-delta', board_handler)
 
         @sio.on('open-orders')
         def open_orders_handler(msg):
