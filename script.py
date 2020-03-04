e1f138131c061a7b3bbbba8943572790e59a3bed
T. Ismael <ismael.verdugo.za@gmail.com>
2020-03-04 17:35:41 -0300

    clean up

--- script.py
+++
@@ -1,8 +0,0 @@
-import cryptomarket.exchange.client as c
-import cryptomarket.exchange.socket as s
-
-client = c.Client("2755964/01BgCj1WCabUw2", "JeufSrS1FoYxD/AOEpRF0XMrwVFFU1OL80qViE94R80Tpz9VZ0Cr5Rl8EXhmX0Yd3HcD89T6S")
-
-auth = client.get_auth_socket()
-
-s.Socket(auth)
